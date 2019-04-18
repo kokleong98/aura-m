f95433bfd80b9b4603faf9f457834971d50e5b0e
SOO KOK LEONG <kokleong98@gmail.com>
2019-04-18 21:44:47 +0800

    Update install-auram.sh

--- install-auram.sh
+++ install-auram.sh
@@ -4,14 +4,24 @@
 # FILENAME=install-auram.sh
 # DESCRIPTION=AURA-M Main installation script
 #################################################################### 
+license=$(curl -s https://raw.githubusercontent.com/kokleong98/aura-m/master/LICENSE)
+echo -e "\e[1;33m$license\e[0m"
+read -p "Do you accept the license agreement? (y/n) " accept
 
-cat << EOF
-*************************************************************************************
-If you are using gmail account please make sure security setting
-  "Allow less secure app: ON"
+if [ accept != 'y' ]; then
+  echo "Aborting installation."
+  exit 0
+fi
 
-Login to gmail acccount and visit following link to check your gmail account setting:
-  https://myaccount.google.com/lesssecureapps
-*************************************************************************************
-EOF
-add-aura.sh
+read -p "Enter new user account: " username
+sudo chmod +x add-aura.sh
+echo "Start Deploy aura."
+add-aura.sh $username
+exitcode=$?
+
+if [ $exitcode -ne 0 ]; then
+  echo "Deploy aura failed."
+  exit exitcode
+else
+  echo "Deploy aura success."
+fi
