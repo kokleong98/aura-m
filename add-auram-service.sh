e0ad03feca33100f343c7ef1ef0bd5bd1ef58ec0
SOO KOK LEONG <kokleong98@gmail.com>
2019-04-19 17:25:36 +0800

    Update add-auram-service.sh

--- add-auram-service.sh
+++ add-auram-service.sh
@@ -4,3 +4,35 @@
 # FILENAME=install-auram.sh
 # DESCRIPTION=AURA-M systemd service installation script
 ####################################################################
+if [ $# -lt 1 ]
+  echo "Insufficient parameters."
+  exit 1
+fi
+username="$1"
+DIR="$2"
+
+if [ -z "$DIR" ]; then
+  DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+fi
+#########################################
+# 4. Create AURA-M systemd service file
+#########################################
+cat > aura-m.service << EOF
+[Unit]
+Description=AURA-M monitoring service
+
+[Service]
+User=$username
+WorkingDirectory=/home/$username/
+ExecStart=${DIR}/start-auram.sh
+ExecStop=${DIR}/stop-auram.sh
+Restart=on-failure
+RestartSec=10s
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo mv aura-m.service /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl enable aura-m.service
