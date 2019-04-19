e2e095202a21bb0cc82bf581835ca8ae055735d5
SOO KOK LEONG <kokleong98@gmail.com>
2019-04-19 17:24:32 +0800

    Update install-auram.sh

--- install-auram.sh
+++ install-auram.sh
@@ -4,6 +4,7 @@
 # FILENAME=install-auram.sh
 # DESCRIPTION=AURA-M Main installation script
 #################################################################### 
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 license=$(curl -s https://raw.githubusercontent.com/kokleong98/aura-m/master/LICENSE)
 echo -e "\e[1;33m$license\e[0m"
@@ -14,6 +15,15 @@ if [ "$accept" != "y" ]; then
   exit 0
 fi
 
+function GetGitDependency(filename)
+{
+  if [ ! -f "${DIR}/$filename" ]
+    echo "Downloading $filename."
+    curl -O "https://raw.githubusercontent.com/kokleong98/aura-m/master/$filename"
+  fi
+  sudo chmod +x "${DIR}/$filename"
+}
+
 #########################################
 # 1. Deploying aura componenets
 #########################################
@@ -22,7 +32,7 @@ read -p "Enter new user account: " username
 Inst_Count=$(dpkg -s apt-transport-https ca-certificates curl software-properties-common docker-compose build-essential python npm | grep "Status: install ok installed" -c)
 
 if [ $Inst_Count -ne 8 ]; then
-  sudo chmod +x add-aura.sh
+  GetGitDependency "add-aura.sh"
   echo "Start Deploy aura."
   "${DIR}/add-aura.sh" $username
   exitcode=$?
@@ -48,6 +58,16 @@ else
     else
       echo "Added $username to group $group sucessfully."
     fi
+	group="sudo"
+    echo "Adding $username to group $group."
+    sudo usermod -aG sudo $username
+    if [ $? -ne 0 ];
+    then
+      echo "Fail to add $username to group $group. Abort installation."
+      exit 1
+    else
+      echo "Added $username to group $group sucessfully."
+    fi
   else
     echo "$username already belongs to group $group."
   fi
@@ -56,26 +76,8 @@ fi
 #########################################
 # 2. Check and prepare AURA-M scripts.
 #########################################
+GetGitDependency "start-auram.sh"
+GetGitDependency "stop-auram.sh"
+GetGitDependency "add-auram-service.sh"
 
-#########################################
-# 4. Create AURA-M systemd service file
-#########################################
-cat > aura-m.service << EOF
-[Unit]
-Description=AURA-M monitoring service
-
-[Service]
-User=$username
-WorkingDirectory=/home/$username/
-ExecStart=${DIR}/start-auram.sh
-ExecStop=${DIR}/stop-auram.sh
-Restart=on-failure
-RestartSec=10s
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-sudo mv aura-m.service /etc/systemd/system/
-sudo systemctl daemon-reload
-sudo systemctl enable aura-m.service
+"${DIR}/add-auram-service.sh" "$username" "$DIR"
