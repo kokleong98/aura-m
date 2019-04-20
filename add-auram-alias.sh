8ca4fdccca98dd8a7a79a2ee2f2c2d0b3d730080
SOO KOK LEONG <kokleong98@gmail.com>
2019-04-20 10:30:37 +0800

    Add files via upload

--- add-auram-alias.sh
+++ add-auram-alias.sh
@@ -4,3 +4,14 @@
 # FILENAME=install-auram-alias.sh
 # DESCRIPTION=AURA-M add command aliases installation script
 ####################################################################
+function auram() {
+  if [ "$1" == "start" ]; then
+    sudo systemctl start auram
+  else if [ "$1" == "stop" ]; then
+    sudo systemctl stop auram
+  else if [ "$1" == "status" ]; then
+    sudo systemctl status auram
+  else if [ "$1" == "logs" ]; then
+    sudo journalctl -u auram -f
+  fi
+}
