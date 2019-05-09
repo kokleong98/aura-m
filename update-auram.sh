56a5d19bf7f929b45ccb7fc513751092343e7c46
SOO KOK LEONG <kokleong98@gmail.com>
2019-05-09 21:52:50 +0800

    Update update-auram.sh

--- update-auram.sh
+++ update-auram.sh
@@ -4,3 +4,12 @@
 # FILENAME=update-auram.sh
 # DESCRIPTION=AURA-M update version services script
 ####################################################################
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+auram stop
+
+sudo rm ~/.auram/start-auram.sh ~/.auram/stop-auram.sh 
+
+curl -O https://raw.githubusercontent.com/kokleong98/aura-m/master/install-auram.sh
+chmod +x install-auram.sh
+sudo ./install-auram.sh
